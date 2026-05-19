998a5e7d0d3d94372997ecb03dfbb7604814ef3e
4D-19\koyo <a>
2026-05-19 15:05:02 +0900

    sharedポインタ

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,17 +1,40 @@
 ﻿
 #include <iostream>
 
-void foo(std::unique_ptr<int> p)
+void unique()
 {
-	std::cout << *p << std::endl;
-}
-
-int main()
-{
-	std::unique_ptr<int> unique(new int(10));
-	std::unique_ptr<int>unique2 ;
+	std::unique_ptr<int> unique(std::make_unique<int>(10));
+	std::unique_ptr<int>unique2;
 	unique2.swap(unique);
 
 	std::cout << *unique2 << std::endl;
+
+}
+
+class Sample
+{
+public:
+	std::shared_ptr<int>value;
+
+	Sample()
+		: value(std::make_shared<int>(20))
+	{
+	}
+
+
+};
+
+void shared()
+{
+	auto sample = new Sample();
+	std::cout << *sample->value << std::endl;
+}
+
+
+
+int main()
+{
+	// unique();
+	shared();
 }
 
