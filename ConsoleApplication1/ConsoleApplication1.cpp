bf5f97062692aab631d79301b37f5452a89e12d6
4D-19\koyo <a>
2026-05-19 14:03:16 +0900

    void型の関数(foo2)の追加（これも問題）

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,10 +7,17 @@ int* foo()
 	return &a;
 }
 
+void foo2()
+{
+	int b;
+	std::cout << &b << std::endl;
+}
+
 
 int main()
 {
 	int* p = foo();
+	foo2();
 
 	std::cout << p << std::endl;
 	std::cout << &p << std::endl;
