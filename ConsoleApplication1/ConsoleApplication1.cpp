680ee37c946f72bbbd45f3289402e30390828753
4D-19\koyo <a>
2026-05-19 13:53:22 +0900

    char型（b,c）の追加

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -3,11 +3,15 @@
 
 int main()
 {
+	char b = 0;
+	char c = 0;
 	int v1 = 10;
 	//int v2 = 20;
 	int* p1 = new int(20);
 	//int* p2 = new float(20);
 	std::cout << &v1 << std::endl;
+	std::cout << (int *)&b << std::endl;
+	std::cout << (int*)&c << std::endl;
 	std::cout << &p1 << std::endl;
 }
 
