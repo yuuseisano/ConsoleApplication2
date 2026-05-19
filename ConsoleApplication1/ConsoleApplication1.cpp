0ad3216e6c03f233aab44cf3e323b1819cce3a1e
4D-19\koyo <a>
2026-05-19 13:57:24 +0900

    スタックとヒープを理解していないときに起きる問題

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -1,17 +1,19 @@
 ﻿
 #include <iostream>
 
+int* foo()
+{
+	int a = 10;
+	return &a;
+}
+
+
 int main()
 {
-	char b = 0;
-	char c = 0;
-	int v1 = 10;
-	//int v2 = 20;
-	int* p1 = new int(20);
-	//int* p2 = new float(20);
-	std::cout << &v1 << std::endl;
-	std::cout << (int *)&b << std::endl;
-	std::cout << (int*)&c << std::endl;
-	std::cout << &p1 << std::endl;
+	int* p = foo();
+
+	std::cout << p << std::endl;
+	std::cout << &p << std::endl;
+
 }
 
