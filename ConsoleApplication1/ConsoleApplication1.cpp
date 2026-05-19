6afb842ace85ef8706cda8349778a08bf1b7c6bf
4D-19\koyo <a>
2026-05-19 14:05:06 +0900

    メモリ破壊の要因

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -7,17 +7,22 @@ int* foo()
 	return &a;
 }
 
-void foo2()
+void foo2(int *p)
 {
-	int b;
+	int b = 10;
 	std::cout << &b << std::endl;
+	std::cout << b << std::endl;
+
+	*p = 20;
+	std::cout << b << std::endl;
+
 }
 
 
 int main()
 {
 	int* p = foo();
-	foo2();
+	foo2(p);
 
 	std::cout << p << std::endl;
 	std::cout << &p << std::endl;
